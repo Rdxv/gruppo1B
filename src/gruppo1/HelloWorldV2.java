9619e6cc24412d8ef37dddfe634c071516e8e128
Rdx <100215987+Rdxv@users.noreply.github.com>
2022-03-10 11:59:50 +0100

    created exercise 2 project

---
+++ src/gruppo1/HelloWorldV2.java
@@ -0,0 +1,40 @@
+package gruppo1;
+
+public class HelloWorldV2 {
+
+	public static void main(String[] args) {
+			
+
+	}
+	
+	public static void Andrea() {
+		
+		System.out.println("Hello World");
+	}
+	
+	public static void Simone() {
+		
+		
+	}
+	
+	public static void Pietro() {
+		
+		
+	}
+	
+	public static void Matteo() {
+		
+		
+	}
+	
+	public static void Stefano() {
+		
+		
+	}
+	
+	public static void Paolo() {
+		
+		
+	}
+	
+}
